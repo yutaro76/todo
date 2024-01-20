1e14c2cacb6c1fcdd9772a12d8110ff629ce292b
yutaro okuda <yutaro.okuda.76@gmail.com>
2024-01-20 16:46:16 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     VitePWA({
@@ -34,6 +35,7 @@ export default defineConfig({
             purpose: 'maskable',
           },
         ],
+        id: '/todo/',
       },
     }),
   ],
